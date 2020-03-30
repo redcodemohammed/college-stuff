6931dc35181465a12d6095a2464b0f06f2e10d46
redcodemohammed <redcodemohammed@gmail.com>
2020-03-30 19:41:27 +0300

    Created Average app

---
+++ Average.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+/**
+ * Average
+ */
+public class Average {
+
+    public static void main(String[] args) {
+        // initialize the scanner:
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("This app will calculate the average of given degrees.");
+
+        // ask for the maximum value:
+        System.out.print("Please enter the maximum possible degree: ");
+        int maximumD = input.nextInt();
+
+        // variables to hold total and number of degrees:
+        int n = 0;
+        int total = 0;
+
+        // the loop that get degrees:
+        System.out.print("Please enter your degree: ");
+        while (input.hasNextInt()) {
+            // get the next int:
+            int nextDegrees = input.nextInt();
+
+            // check if ins value is bigger than the maximum:
+            if (nextDegrees > maximumD) {
+                System.out.print("Given degree is bigger than the maximum, please try again: ");
+                continue;
+            }
+
+            // increase n and add the degree to the total:
+            n++;
+            total += nextDegrees;
+
+            // ask the user for the next degree
+            System.out.print("Please enter your degree, or type done when done adding degrees: ");
+        }
+        if (n == 0)
+            System.out.println("You didn't enter any degrees.");
+        else
+            System.out.println("Your average is: " + (total / n));
+        input.close();
+    }
+}
