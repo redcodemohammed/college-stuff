e42bea11b435afb27c9d611dbc10601059ac6935
redcodemohammed <redcodemohammed@gmail.com>
2020-04-06 13:31:52 +0300

    2 homeworks done

---
+++ Homework1.java
@@ -0,0 +1,17 @@
+/**
+ * Homework1
+ */
+public class Homework1 {
+
+    public static void main(String[] args) {
+        int age = 18;
+        char x = 'x';
+        double z = 19.17;
+        String name = "Mohammed";
+
+        System.out.println(age);
+        System.out.println(x);
+        System.out.println(z);
+        System.out.println(name);
+    }
+}
---
+++ Homework2.java
@@ -0,0 +1,7 @@
+
+public class Homework2 {
+    public static void main(String[] args) {
+        int x = ((5 - 2) / 3) + (2 * 6);
+        System.out.println(x);
+    }
+}
