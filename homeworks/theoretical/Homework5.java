f5be147460857999f5c51cdf310ecb6f0c56e2b5
Mohammed <51872890+redcodemohammed@users.noreply.github.com>
2020-04-24 14:00:53 +0300

    Create Homework5.java

---
+++ homeworks/theoretical/Homework5.java
@@ -0,0 +1,10 @@
+public class Homework5 {
+  public static void main(String[] args) {
+  	int m = 5;
+		int n = 10;
+		if (m < n)
+			++m;
+			++n;
+			System.out.println(" m = " + m + " n = " + n);
+  }
+}
