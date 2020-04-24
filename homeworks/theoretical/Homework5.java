77e5d88b2c3d5df4d9666e0a7eb963e2e98bd0b8
redcodemohammed <redcodemohammed@gmail.com>
2020-04-24 20:31:09 +0300

    added Homework5.java to launch.json

--- homeworks/theoretical/Homework5.java
+++ homeworks/theoretical/Homework5.java
@@ -1,10 +1,12 @@
+package homeworks.theoretical;
+
 public class Homework5 {
-  public static void main(String[] args) {
-  	int m = 5;
-	int n = 10;
-	if (m < n)
-		++m;
+	public static void main(String[] args) {
+		int m = 5;
+		int n = 10;
+		if (m < n)
+			++m;
 		++n;
-	System.out.println(" m = " + m + " n = " + n);
-  }
+		System.out.println(" m = " + m + " n = " + n);
+	}
 }
